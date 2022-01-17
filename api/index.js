9970553d8a62a0910eeb6750e0982208ce36effe
luisRocca <luis.rocca96@gmail.com>
2022-01-16 21:34:05 -0500

    ultimos retoques}

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const port = app.get("PORT");
 // middleweares
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://pb-shopify-ec.vercel.app/'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST');
